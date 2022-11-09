ece7d9e241e78163e91e01dbf18bdfd73db33852
Pedro-servoDeCristo <107230091+Pedro-servoDeCristo@users.noreply.github.com>
2022-11-08 21:03:29 -0300

    Update Git_questao3_stackUCB.c

--- Git_questao3_stackUCB.c
+++ Git_questao3_stackUCB.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-
+    int catetoB = 4, catetoC = 3, hipotenusa;
+    
+    hipotenusa = sqrt(catetoC*catetoC + catetoB*catetoB);
+    
+    printf("%d", hipotenusa);
+    
     return 0;
 }
